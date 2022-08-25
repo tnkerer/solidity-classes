3011cae153f060b35c1e9463aaae814d3b115c1d
menezesphill <phillipe.o.m@hotmail.com>
2022-08-25 19:44:55 -0300

    code comment fixes

--- class-02/contracts/Token.sol
+++ class-02/contracts/Token.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract JaumCoin is ERC20 {
     constructor() ERC20("JaumCoin", "JC") {
-        _mint(msg.sender, 1000 * 10 ** decimals()); // 0.0000000000000001
+        _mint(msg.sender, 1000 * 10 ** decimals());
     }
 
     // uint256
     // By default, decimals are equal to 18
 
     // If I want to create 1000 Coins
-    // To the EVM, this number is 1000000000000000000000 / 1e18 = 1000
+    // To the EVM, this number is 1000000000000000000000
 }
